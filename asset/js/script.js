746b25b6ca46688e48c3c5c79eccf882789beb74
rabeeajahangir <69980221+rabeeajahangir@users.noreply.github.com>
2021-05-23 15:04:46 -0400

    added pseudo code

---
+++ asset/js/script.js
@@ -0,0 +1,11 @@
+//Current day displayeyed when the planner is opened
+
+//Standard time blocks for business hours
+
+// Eacg time block is color coded to indicate past, present or futire appointment
+
+//Click on the block to create the event
+
+//Click to save event
+
+//Saved events persist when the page is refreshed.
