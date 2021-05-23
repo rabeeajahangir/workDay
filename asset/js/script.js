1ac04b68e4ae7cd183a00e4c251490a4be9eb214
rabeeajahangir <69980221+rabeeajahangir@users.noreply.github.com>
2021-05-23 15:26:15 -0400

    display date

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,5 +1,12 @@
-//Current day displayeyed when the planner is opened
+//Current day displayed when the planner is opened
+function displayDate() {
+    var displayDayEl = document.querySelector("#currentDay");
 
+    var currentDay = moment().format("dddd[,] MMMM Do[,] YYYY");
+    displayDayEl.textContent = currentDay;
+    console.log(currentDay);
+    
+}
 //Standard time blocks for business hours
 
 // Eacg time block is color coded to indicate past, present or futire appointment
